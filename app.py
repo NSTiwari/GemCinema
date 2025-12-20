8d3c1b3dbbfcc2d7b143409614ff7bc44d212b22
Nitin Tiwari <37960279+NSTiwari@users.noreply.github.com>
2025-12-21 01:51:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -247,7 +247,7 @@ def generate_video():
         operation = client.models.generate_videos(
             model="veo-3.1-generate-preview",
             prompt=full_video_prompt,
-            config=types.GenerateVideosConfig(reference_images=refs), resolution="1080p"
+            config=types.GenerateVideosConfig(reference_images=refs)
         )
         while not operation.done:
             time.sleep(5)
